2fda2d07e70adbf3fad30c24e3f47845d13d145f
Siddhi Gavhane <siddhigavhane03@gmail.com>
2026-04-18 11:12:31 +0530

    via upload

---
+++ programs/OddEven.java
@@ -0,0 +1,15 @@
+package programs;
+import java.util.*;
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n = sc.nextInt();
+        if( n%2 == 0){
+            System.out.println(n+" Even Number");
+        }else{
+            System.out.println(n+" Odd Number");
+        }
+    }
+    
+}
