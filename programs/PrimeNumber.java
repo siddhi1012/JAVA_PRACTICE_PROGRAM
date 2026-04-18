7b90014c2e9882ab91d9d18f57c0aed87d1ab3d0
Siddhi Gavhane <siddhigavhane03@gmail.com>
2026-04-18 11:15:21 +0530

    via upload

---
+++ programs/PrimeNumber.java
@@ -0,0 +1,12 @@
+package programs;
+
+import java.util.*;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n = sc.nextInt();
+    }
+
+}
